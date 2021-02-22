f90600efcfc0365e22756606377153407ea27fc3
ekaterina <smirnovaev@lad24.ru>
2021-02-22 12:19:50 +0300

    create docker-compose

--- server/server.js
+++ server/server.js
@@ -45,12 +45,3 @@ const init = async () => {
     await server.start();
     console.log(`Server running at: ${server.info.uri}/documentation`);
 };
-
-
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
-});
-
-init();
